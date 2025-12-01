87bbc38136de85c81cf1d20c0ce75d15cf00bb4e
DerekA-325 <derave1577@students.ecpi.edu>
2025-11-30 23:53:14 -0500

    Add files via upload
    Added the conversion functions

--- Project menu.py
+++ Project menu.py
@@ -1,3 +1,19 @@
+def convertData(var1):
+    conv = var1*2.54
+    return conv
+
+def getInput():
+    i = 0
+    x = int(input("How many entries are being input?: "))
+    while (i < x):
+        date = str(input("Enter a date: "))
+        temp = int(input("Enter a length in inches: "))
+        # Calling the convertData function, expecting an integer return value.
+        tempC = convertData(temp)
+        print("The following was saved on ",str(datetime.now()))
+        print(f'{date}, {temp}in, {tempC}cm')
+        i = i+1
+
 print("derave1577 Spreadsheet Automation Menu")
 print("")
 print("Choose a number from the following programs")
@@ -15,14 +31,20 @@ match x:
         print("")
         print('You selected input data.')
         print('The date and time is',str(datetime.now()))
+        getInput()
     case 2:
         print("")
         print('You selected view current data.')
         print('The date and time is',str(datetime.now()))
+        print("This feature is not currently available.")
     case 3:
         print("")
         print('You selected generate report.')
         print('The date and time is',str(datetime.now()))
+        print("This feature is not currently available.")
     case _:
         print("")
         print("Invalid input")
+
+
+        
